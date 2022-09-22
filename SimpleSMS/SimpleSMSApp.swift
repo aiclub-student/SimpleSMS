b26b7c7bfdc1d530ba65323459872a3dca4cd7f7
Amit Gupta <amit@gprof.com>
2022-09-22 11:56:31 -0700

    Initial Commit

---
+++ SimpleSMS/SimpleSMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSMSApp.swift
+//  SimpleSMS
+//
+//  Created by Amit Gupta on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
